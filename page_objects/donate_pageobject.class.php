2300027ecf4477d555cbdac9baa03606225e750d
GodMod <godmod23@web.de>
2017-03-13 21:00:16 +0100

    Fix

--- page_objects/donate_pageobject.class.php
+++ page_objects/donate_pageobject.class.php
@@ -300,13 +300,13 @@ class donate_pageobject extends pageobject
 
 	//Template Vars
 	$this->tpl->assign_vars(array(
-		'DONATE_DD_PUBLIC' 		=> (new hdropdown('public', array('options' => array(1 => $this->user->lang('donations_public'), 0 => $this->user->lang('donations_hide_name')), 'value' => 1)))->output(),
+		'DONATE_DD_PUBLIC' 			=> (new hdropdown('public', array('options' => array(1 => $this->user->lang('donations_public'), 0 => $this->user->lang('donations_hide_name')), 'value' => 1)))->output(),
 		'DONATION_PLACEHOLDER' 		=> $this->config->get('min_value', 'donations'),
-		'DONATION_TEXT'			=> $this->bbcode->toHTML($this->config->get('donation', 'donations')))->output(),
+		'DONATION_TEXT'				=> $this->bbcode->toHTML($this->config->get('donation', 'donations')),
 		'DONATION_CURRENCY' 		=> $this->config->get('paypal_currency', 'donations'),
-		'S_DONATION_FREE' 		=> ($this->config->get('free_or_custom', 'donations') == 'free') ? true : false,
+		'S_DONATION_FREE' 			=> ($this->config->get('free_or_custom', 'donations') == 'free') ? true : false,
 		'DD_DONATION_AMOUNTS' 		=> (new hdropdown('value', array('options' => $arrCustomAmounts)))->output(),
-		'S_CAN_DONATE'			=> $this->user->check_auth('u_donations_donate', false),
+		'S_CAN_DONATE'				=> $this->user->check_auth('u_donations_donate', false),
 	));
 	
 	//CAPTCHA
