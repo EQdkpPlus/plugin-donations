f88ecb9b20d394a9ddef70e85080426792cb1457
GodMod <GodMod@users.noreply.github.com>
2021-02-01 08:12:58 +0100

    Update donations_plugin_class.php

--- donations_plugin_class.php
+++ donations_plugin_class.php
@@ -41,7 +41,7 @@ class donations extends plugin_generic
     return array_merge(parent::$shortcuts, $shortcuts);
   }
 
-  public $version    = '1.0.7';
+  public $version    = '1.0.8';
   public $build      = '';
   public $copyright  = 'GodMod';
   public $vstatus    = 'Stable';
